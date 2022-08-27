9acd1710be95cc586975630f5ffdc1da62c9d637
Mo-Seleman <92472515+Mo-Seleman@users.noreply.github.com>
2022-08-27 14:56:04 +0100

    Removed Top Btn
    No Longer Required

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 const contactArea = document.querySelector('.contact');
 const contactMeTitle = document.querySelector('.title');
 const socialsNav = document.querySelector('#socialsNav');
-const mybutton = document.getElementById("myBtn");
+const navBtn = document.getElementById('mainNav');
+const nav = document.getElementById('navbarNavAltMarkup');
 
 contactArea.addEventListener('click', e => {
     const contactButton = e.target.closest('button');
@@ -11,23 +12,6 @@ contactArea.addEventListener('click', e => {
     }
 );
 
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0; 
-}
-
-const navBtn = document.getElementById('mainNav');
-const nav = document.getElementById('navbarNavAltMarkup');
 navBtn.addEventListener('click', () => {
   nav.classList.contains('show') ? nav.classList.remove('show') : nav.classList.add('show');
 });
